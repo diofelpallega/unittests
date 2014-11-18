d3f798dfbe3394ed5a0483edd0f369d5d9113601
supersherlock <diofel.pallega@gmail.com>
2014-11-18 20:14:49 +0800

    added unittests for withdraw and added codes to verify data type of inputs

---
+++ tests/account_test.py
@@ -0,0 +1,18 @@
+import unittest
+from account import Account
+from account import AccountWithdraw
+
+
+class TestAccount(unittest.TestCase):
+    def test_account_object_can_be_created(self):
+        account = Account("001",50)
+        self.assertEqual(account.account_number,"001")
+        self.assertEqual(account.balance,50)
+ 
+    def test_account_object_can_withdraw(self):
+        account = AccountWithdraw("001",50,5)
+        self.assertEqual(account.balance,45)
+    
+        
+if __name__== '__main__':
+    unittest.main()
