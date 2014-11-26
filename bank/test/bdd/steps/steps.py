5b9b830c748c4273c991f12fff213796e01dc011
supersherlock <diofel.pallega@gmail.com>
2014-11-26 09:05:08 +0800

    [Done] bank activity assignment 2

--- bank/test/bdd/steps/steps.py
+++ bank/test/bdd/steps/steps.py
@@ -17,6 +17,7 @@ def when_i_enter_the_account_number_group1(step, account_number):
     world.form_response = form.submit()
     assert_equal(world.form_response.status_code, 200)
 
+
 @step(u'Then I see a balance of "([^"]*)"')
 def then_i_see_a_balance_of_group1(step, expected_balance):
     assert_in("Balance: {}".format(expected_balance),world.form_response.text)
@@ -27,3 +28,11 @@ def i_create_account_with_balance_of_group1(step, account_number,
 
     a = Account(account_number,  balance )
     BANK.add_account(a)
+
+@step(u'I create the following account:')
+def i_create_the_following_account(step):
+    for row in step.hashes:
+        a = Account(row['account_number'], row['balance'])
+        BANK.add_account(a)
+
+
